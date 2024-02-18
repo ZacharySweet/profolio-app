330e410bb599d5e16fe9db67912f686c73046f4a
Elijah <elijah@vanshardware.com>
2024-02-18 13:24:07 -0600

    BIG PLAYS

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,8 +7,49 @@ void main() {
   ));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  
+
+  String avatarImage = 'assets/van-orang.jpg';
+
+  void pictureSelect(String titleText) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(titleText),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.photo),
+                title: const Text("Choose from Gallery"),
+                onTap: () {
+                  // Implement functionality to choose image from gallery
+                  Navigator.pop(context); // Close the dialog
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.camera),
+                title: const Text("Take a Photo"),
+                onTap: () {
+                  // Implement functionality to take a photo
+                  Navigator.pop(context); // Close the dialog
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,21 +74,27 @@ class MainApp extends StatelessWidget {
               Stack(
                 alignment: Alignment.topCenter,
                 children: [
-                  const Expanded(
-                    child: SizedBox(height: 180,)),
-                  Expanded(
-                    child: Container(
-                      height: 120,
-                      color: Colors.black,
-                    )
+                  const SizedBox(height: 180,),
+                  Container(
+                  height: 120,
+                  decoration: 
+                    BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(avatarImage), fit: BoxFit.fill
+                      )
+                    ),
                   ),
 
-                  const Positioned(
+                  Positioned(
                     top: 50,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.amber,
-                      radius: 50,
-                      )
+                    child: GestureDetector(
+                      onTap: () {pictureSelect("Change Avatar");
+                      print('circleAvatar tapped');},
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(avatarImage),
+                        radius: 50,
+                        ),
+                    )
                     )
                 ],
                 
