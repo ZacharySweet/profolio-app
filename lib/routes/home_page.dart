e5ebacd526e8c400f1a7980c1d6cdbaa1d3dbe1c
BuildTools <unconfigured@null.spigotmc.org>
2024-04-12 00:04:45 -0500

    home page work

--- lib/routes/home_page.dart
+++ lib/routes/home_page.dart
@@ -3,31 +3,54 @@ import 'package:profolio/routes/test_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Makes the main page
     return MaterialApp(
-        home: Scaffold(
-      // Creates the appbar
-      appBar: AppBar(
-        centerTitle: true,
-        shadowColor: const Color.fromARGB(0, 255, 255, 255),
-        backgroundColor: const Color.fromARGB(0, 255, 255, 255),
-        leading: const CircleAvatar(
-            backgroundImage: AssetImage('lib/assets/images/van-orang.jpg')),
-        title: const Text(
-          "HOME",
-          style: TextStyle(color: Colors.black),
+      home: Scaffold(
+        // Creates the appbar
+        appBar: AppBar(
+            centerTitle: true,
+            shadowColor: const Color.fromARGB(0, 255, 255, 255),
+            backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+            title: const Row(
+              children: [
+                FlutterLogo(),
+                SizedBox(width: 16),
+                Text("Your Profolio",
+                    style: TextStyle(
+                        color: Colors.black, fontSize: 26, fontFamily: 'Fun'))
+              ],
+            )),
+        body: const Column(
+          children: [
+            // Making the navigation bar
+            DefaultTextStyle(
+                style: TextStyle(color: Colors.black, fontFamily: 'WorkSans'),
+                child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 35.0, vertical: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Info"),
+                        Text("Academics"),
+                        Text("Sports"),
+                        Text("Clubs")
+                      ],
+                    )))
+          ],
+        ),
+        // Creates the main action button used for editing actions in the app
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const TestPage()));
+          },
+          child: const Icon(Icons.add),
         ),
       ),
-      // Creates the main action button used for editing actions in the app
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const TestPage()));
-        },
-        child: const Icon(Icons.add),
-      ),
-    ));
+    );
   }
 }
