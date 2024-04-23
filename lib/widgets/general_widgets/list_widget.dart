6ccb2e90777d264a570ce645acef8d7ba2dad106
BuildTools <unconfigured@null.spigotmc.org>
2024-04-23 14:10:11 -0500

    PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE

--- lib/widgets/general_widgets/list_widget.dart
+++ lib/widgets/general_widgets/list_widget.dart
@@ -5,9 +5,28 @@ class ListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(decoration: const BoxDecoration(color: Color.fromARGB(255, 255, 255, 255), borderRadius: BorderRadius.all(Radius.circular(10)), boxShadow: [BoxShadow(color: Colors.black)]), child: const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Row(children: [FlutterLogo(size: 40), Column(children: [Text("Title"), Text("Other Title")])],),
-    ));
+    return Container(
+        decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                  color: Color.fromARGB(29, 0, 0, 0),
+                  spreadRadius: 0.01,
+                  blurRadius: 2)
+            ]),
+        child: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              FlutterLogo(size: 40),
+              SizedBox(width: 10),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text("Title", textAlign: TextAlign.left),
+                Text("Other Title", textAlign: TextAlign.left)
+              ])
+            ],
+          ),
+        ));
   }
 }
